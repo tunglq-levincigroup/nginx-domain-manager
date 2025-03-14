6fe76dab8b79932953db5dec69f8549a147c548d
leetun2k2 <lequangtung2002@gmail.com>
2025-03-14 15:02:29 +0700

    feat: regiter ssl include redirection

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request
+from controllers.config import add_config_controller, remove_config_controller
+from controllers.dns import check_dns_controller
 from utils.request import get_request_data
 from utils.response import api_response
 from utils.auth import authorize
@@ -16,6 +18,52 @@ def index():
     """Handle GET request to the root endpoint."""
     return index_controller()
 
+#####################################################################
+################    Register + redirect     #########################
+#####################################################################
+@app.route('/dns/check', methods=['POST'])
+def check_dns():
+    api_key = request.headers.get('X-API-KEY')
+    status, message = authorize(api_key)
+    if not status:
+        return api_response(401, message)
+
+    status, data = get_request_data(request.json, 'domains')
+    if not status:
+        return api_response(400, "Invalid parameters.")
+
+    return check_dns_controller(data['domains'])
+
+@app.route('/config/add', methods=['POST'])
+def add_config():
+    api_key = request.headers.get('X-API-KEY')
+    status, message = authorize(api_key)
+    if not status:
+        return api_response(401, message)
+
+    status, data = get_request_data(request.json, 'base_domain', 'redirect_domain', 'target_domain')
+    if not status:
+        return api_response(400, "Invalid parameters.")
+
+    return add_config_controller(data['base_domain'], data['redirect_domain'], data['target_domain'])
+
+@app.route('/config/remove', methods=['DELETE'])
+def remove_config():
+    api_key = request.headers.get('X-API-KEY')
+    status, message = authorize(api_key)
+    if not status:
+        return api_response(401, message)
+    
+    status, data = get_request_data(request.json, 'target_domain')
+    if not status:
+        return api_response(400, "Invalid parameters.")
+
+    return remove_config_controller(data['target_domain'])
+
+################ This code is not up to date ########################
+#####################################################################
+################    Register                #########################
+#####################################################################
 @app.route('/domain/add', methods=['POST'])
 def add_domain():
     """Handle POST request to add a new domain."""
--- services/template.py
+++ services/template.py
@@ -1,3 +1,78 @@
+
+############################# latest function to config ssl
+def generate_nginx_ssl_conf_with_redirect(base_domain: str, redirect_domain: str, target_domain: str) -> str:
+    """
+    Generates an Nginx configuration for a given domain.
+
+    Args:
+        base_domain         (str): The base domain or upstream server.
+        redirect_domain:    (str): The domain that redirect to target_domain when accessing.
+        target_domain       (str): The domain for which the config is generated.
+
+    Returns:
+        str: The generated Nginx configuration.
+    """
+    return f"""# Generated for {target_domain} from the {base_domain} config template
+# Domain {redirect_domain} with redirect to {target_domain}
+
+# Main config
+server {{
+    listen 443 ssl;
+    server_name {target_domain};
+
+    access_log /var/log/nginx/{target_domain}.log;
+    error_log /var/log/nginx/{target_domain}.error.log;
+
+    ssl_trusted_certificate /etc/letsencrypt/live/{target_domain}/cert.pem;
+    ssl_certificate /etc/letsencrypt/live/{target_domain}/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/{target_domain}/privkey.pem;
+    ssl_protocols             TLSv1.1 TLSv1.2 TLSv1.3;
+    ssl_prefer_server_ciphers on;
+    ssl_ciphers               "EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH";
+    ssl_ecdh_curve            secp384r1;
+    ssl_session_cache         shared:SSL:10m;
+    ssl_session_tickets       off;
+    ssl_stapling              on;
+    ssl_stapling_verify       on;
+
+    location / {{
+        proxy_ssl_server_name on;
+        proxy_pass https://{base_domain};
+
+        location / {{
+            proxy_pass https://{base_domain};
+        }}
+        try_files $uri $uri/ =404;
+    }}
+}}
+
+# Redirect from http://{target_domain} to https://{target_domain}
+server {{
+    listen 80;
+    server_name {target_domain};
+    return 301 https://{target_domain}$request_uri;
+}}
+
+# Redirect from http://{redirect_domain} to https://{target_domain}
+server {{
+    listen 80;
+    server_name {redirect_domain};
+    return 301 https://{target_domain}$request_uri;
+}}
+
+# Redirect from https://{redirect_domain} to https://{target_domain}
+server {{
+    listen 443 ssl;
+    server_name {redirect_domain};
+
+    ssl_certificate /etc/letsencrypt/live/{redirect_domain}/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/{redirect_domain}/privkey.pem;
+
+    return 301 https://{target_domain}$request_uri;
+}}
+"""
+
+############################# for domain controller, not up to date
 def generate_nginx_conf(base_domain: str, domain: str) -> str:
     """
     Generates an Nginx configuration for a given domain.
@@ -25,6 +100,7 @@ server {{
 }}
 """
 
+############################# for domain controller, not up to date
 def generate_nginx_ssl_conf(base_domain: str, domain: str) -> str:
     """
     Generates an Nginx configuration for a given domain.
